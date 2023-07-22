fc45dd95a51dbbd72e293136868d830d2bf76315
agyago <agyago@gmail.com>
2023-07-22 19:33:38 +0800

    cidrsubnet changed

--- stack-cheezychinito/main.tf
+++ stack-cheezychinito/main.tf
@@ -2,4 +2,6 @@ module "init_vpc" {
     source = "../modules/VPC"
     cidr_block = var.cidr_block
     count_number = var.count_number
+    priv_count = var.priv_count
+    pub_count = var.pub_count
 }
--- stack-cheezychinito/variables.tf
+++ stack-cheezychinito/variables.tf
@@ -1,2 +1,4 @@
 variable "cidr_block" {}
 variable "count_number" {}
+variable "priv_count" {}
+variable "pub_count" {}
