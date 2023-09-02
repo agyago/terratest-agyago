08817b053a628a949b31911f1db751cfa2d20a57
agyago <42577978+agyago@users.noreply.github.com>
2023-09-02 19:01:08 +0100

    Update lambda_reduce-cost.py
    adding some logic

--- lambda_reduce-cost.py
+++ lambda_reduce-cost.py
@@ -2,6 +2,15 @@ import json
 
 def process_message(message):
    print("SNS message: "+ message)
+   alarm_name = message.get("AlarmName")
+   if not alarm_name:
+      print("ignoring..")
+      return
+   if 'sleepy' in alarmname:
+      print('host will shutdown')
+   else:
+      print('unknown alarm')
+   return
    
 
 def lambda_handler(event, context):
